9ac15b5b85fc7cbb671334a6baea0f14d3e61e7a
KentaGoto <gotokentag@gmail.com>
2020-12-15 11:34:14 +0900

    update

--- srv.pl
+++ srv.pl
@@ -1,6 +1,10 @@
 use strict;
 use warnings;
 use IO::Socket;
+use Time::Piece;
+
+my $t = localtime;
+my $daytime = $t->strftime('%Y-%m-%d %H:%M:%S');
 
 my $s = new IO::Socket::INET(
     Listen    => 5,
@@ -14,8 +18,8 @@ die "Error: $!" unless $s;
 print "Running...\n";
 
 while ( my $c = $s->accept ) {
-    print "Connect.\n";
-    $c->print("Alive: ");
+    print "Connect: $daytime\n";
+    $c->print("Alive: $daytime\n");
     $c->close;
 }
 
--- user.pl
+++ user.pl
@@ -1,10 +1,6 @@
 use strict;
 use warnings;
 use IO::Socket;
-use Time::Piece;
-
-my $t = localtime;
-my $daytime = $t->strftime('%Y-%m-%d %H:%M:%S');
 
 my $s = new IO::Socket::INET(
     PeerAddr => 'localhost',
@@ -14,5 +10,5 @@ my $s = new IO::Socket::INET(
 
 die "IO::Socket : $!" unless $s;
 
-print <$s> . $daytime . "\n";
+print <$s>;
 close($s);
